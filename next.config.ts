0c3c4addb39359fb10ffbdf22a3655337aec3adb
Kemo Krpo <162745688+kemokrpo@users.noreply.github.com>
2025-02-26 03:45:20 +0100

    4th

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   distDir: 'out',
   trailingSlash: true, // Ensures compatibility with GitHub Pages
+  basePath: '/jobfair-site', // Replace with your GitHub repository name
 };
 
 export default nextConfig;
