a8887f59a002c04a7e6d2c88b6ceb362056736cc
Kemo Krpo <162745688+kemokrpo@users.noreply.github.com>
2025-03-11 21:13:40 +0100

    Redeploy?

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -19,7 +19,7 @@ const Agenda: React.FC = () => {
         <div className="p-8 bg-white rounded-lg shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
           <h3 className="text-xl font-semibold text-blue-700 mb-4">Day One</h3>
           <p className="text-gray-600 text-lg leading-relaxed">
-            Nihil est in intellectu quod non prius fuerit in sensu. Amor vincit
+            Nihil est in intellectu quoad non prius fuerit in sensu. Amor vincit
             omnia, et nos cedamus amori.
           </p>
         </div>
