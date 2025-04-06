41d60e0feacfeb6f030ae72eed621ab4feb5aeb9
Nudzejma6 <nudzejma.lopo3@gmail.com>
2025-04-06 14:25:52 +0200

    ,

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -19,7 +19,17 @@ const Agenda: React.FC = () => {
         <div className="p-8 bg-white rounded-lg shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
           <h3 className="text-xl font-semibold text-blue-700 mb-4">Day One</h3>
           <p className="text-gray-600 text-lg leading-relaxed">
-            TBA
+            ~ Otvaranje (9:00)<br></br>
+            ~ Radionica 1 (10:00 -11:30) <br></br>
+            ~ Pauza (11:30-12:00)<br></br>
+            ~ Radionica 2 (12:00 - 13:30)<br></br>
+            ~ Zatvaranje (14:00)<br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            g
+
           </p>
         </div>
 
@@ -27,7 +37,13 @@ const Agenda: React.FC = () => {
         <div className="p-8 bg-white rounded-lg shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
           <h3 className="text-xl font-semibold text-blue-700 mb-4">Day Two</h3>
           <p className="text-gray-600 text-lg leading-relaxed">
-            TBA
+          ~ Registracija učesnika (9:30 - 10:00)<br></br>
+            ~ Otvaranje akademije (10:00 - 10:30) <br></br>
+            ~ Radionica Marketing i brendiranje za StartUp-ove Adela Hadžić(10:30-11:45)<br></br>
+            ~ Pauza za kafu (11:45 - 12:15)<br></br>
+            ~ Radionica Kako privući i zadržati klijente: Savjeti za dugoročni poslovni uspjeh Mesud Pašić (12:15 - 13:30)<br></br>
+            ~ Pauza za ručak (13:30 - 14:30) <br></br>     
+            ~ Radionica Od kontakta do kapitala: Kako do mentora, mreže i finansiranja? Muniz Karajbić (14:30 -15:30) <br></br>
           </p>
         </div>
 
@@ -35,7 +51,14 @@ const Agenda: React.FC = () => {
         <div className="p-8 bg-white rounded-lg shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
           <h3 className="text-xl font-semibold text-blue-700 mb-4">Day Three</h3>
           <p className="text-gray-600 text-lg leading-relaxed">
-            TBA
+          ~ Registracija učesnika (9:30 - 10:00)<br></br>
+            ~ (Samo)motivacija Mia Dugandžić (10:00 - 11:15) <br></br>
+            ~ Pauza za kafu(11:15-11:45)<br></br>
+            ~ Biznis modeli i strategije monetizacije Goran Dodig (11:45 - 13:00)<br></br>
+            ~ Pauza za ručak (13:00 - 14:00)<br></br>
+            ~ Najčešće greške na početku i kako ih ispraviti Elvis Šabić (14:00 - 15:30) <br></br>     
+            ~ Zatvaranje akademije (15:30 -16:00) <br></br>
+         
           </p>
         </div>
       </div>
--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -39,7 +39,8 @@ const Partners: React.FC = () => {
           {[
             { name: "SMED Engineering", src: "/slike/smedeng.png", extraText: "x5" },
             { name: "Telus Digital", src: "/slike/telus.png", extraText: "" },
-            { name: "AtlantBH", src: "/slike/atlant.png", extraText: "x2" },
+            { name: "NLB", src: "/slike/atlant.png", extraText: "x2" },
+            { name: "Bright Moon Digital", src: "/slike/atlant.png", extraText: "x2" },
             { name: "UniCredit Bank", src: "/slike/unicredit.png", extraText: "x4" },
           ].map((partner) => (
             <div
@@ -87,6 +88,36 @@ const Partners: React.FC = () => {
             </div>
             
           </div>
+
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">Intera</h3>
+            {/* Image Container with maximum size control */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/BHFF.png"
+                alt="BHFF"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">NSoft</h3>
+            {/* Image Container with maximum size control */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/BHFF.png"
+                alt="BHFF"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
         </div>
       </section>
 
@@ -100,6 +131,92 @@ const Partners: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mx-12">
           <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
             <h3 className="text-xl font-semibold text-blue-700 mb-4">Bolji Posao</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">Bljesak info</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">STUDOMAT</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">Radio Ilijaš</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">Buka d.o.o.</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">RTV Gradiška</h3>
+            {/* Image Container with maximum size control Promjena komentara */}
+            <div className="w-full flex justify-center max-w-[200px]">
+              <Image
+                src="/slike/boljiposao.jpeg"
+                alt="Bolji Posao"
+                width={200}
+                height={200}
+                className="object-contain max-w-full max-h-[150px]"
+              />
+            </div>
+            
+          </div>
+          <div className="p-8 bg-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out flex flex-col items-center aspect-square max-w-sm mx-auto">
+            <h3 className="text-xl font-semibold text-blue-700 mb-4">Radio M i Radio M plus</h3>
             {/* Image Container with maximum size control Promjena komentara */}
             <div className="w-full flex justify-center max-w-[200px]">
               <Image
